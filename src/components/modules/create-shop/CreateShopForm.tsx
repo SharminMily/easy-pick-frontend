e5173c89ef9ac152e320df1e3dbcbf2eaaae16cc
SharminMily <sharminakther5599@gmail.com>
2025-04-08 21:28:12 +0600

    n

--- src/components/modules/create-shop/CreateShopForm.tsx
+++ src/components/modules/create-shop/CreateShopForm.tsx
@@ -41,7 +41,7 @@ export default function CreateShopForm() {
   
         const res = await createShop(formData);
   
-        console.log(res);
+       // console.log(res);
   
         if (res.success) {
           toast.success(res.message);
